9836bfee2967eb2fb96a6850795f45cfbf4e74f5
khorcheni <khorcheni.essra@gmail.com>
2025-12-17 23:43:51 +0100

    Prepare Flask backend for deployment

--- flashcards-flask/app.py
+++ flashcards-flask/app.py
@@ -1,17 +1,21 @@
-
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials
-cred = credentials.Certificate(r"C:\flashcardsR\flashcards-flask\flashcards-react-53345-firebase-adminsdk-fbsvc-43be6c2223.json")
-firebase_admin.initialize_app(cred)
 from flask import Flask
 from flask_cors import CORS
 from routes.decks import deck_bp
 
+# Load Firebase Admin credentials from environment variable
+firebase_json = os.environ.get("FIREBASE_ADMIN_JSON")
+cred = credentials.Certificate(json.loads(firebase_json))
+firebase_admin.initialize_app(cred)
 
-
+# Initialize Flask app
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins":  ["https://flashcards-react-53345.web.app"]}})  # allow all origins for /api/*
- # allow cross-origin requests
+
+# Enable CORS only for your React frontend
+CORS(app, resources={r"/api/*": {"origins": ["https://flashcards-react-53345.web.app"]}})
 
 # Register blueprints
 app.register_blueprint(deck_bp, url_prefix='/api/decks')
